f5be54aefd879b54963fc870c383829514244520
siren0413 <siren0413@gmail.com>
2016-09-12 00:11:35 -0700

    half complete chapter 7 "http"

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -34,4 +34,8 @@ export class HeroDetailComponent implements OnInit {
         window.history.back();
     }
 
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(this.goBack);
+    }
 }
